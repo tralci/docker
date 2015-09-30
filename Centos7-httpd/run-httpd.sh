d3626a921b0761c3e5af5e28dad625bbc6a2b22d
tralci <fernando.tralci@gmail.com>
2015-09-30 15:04:14 -0300

    3 commit

--- Centos7-httpd/run-httpd.sh
+++ Centos7-httpd/run-httpd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
